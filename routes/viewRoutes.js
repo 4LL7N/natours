02b1343dfd918814cd2291a356bf8fcdb78d6e2c
nika <nika3141535@gmail.com>
2024-07-20 09:55:57 +0400

    implemented cors

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,21 +1,30 @@
 const express = require('express');
-const {getOverview,getTour,Login,getAccount,updateUserData,getMyTours, Signup} = require('../controllers/viewController')
-const {isLoggedIn, protect} = require('../controllers/authController')
-const {createBookingCheckout} = require('../controllers/bookingController')
-
+const {
+  getOverview,
+  getTour,
+  Login,
+  getAccount,
+  updateUserData,
+  getMyTours,
+  Signup,
+} = require('../controllers/viewController');
+const { isLoggedIn, protect } = require('../controllers/authController');
+// const { createBookingCheckout } = require('../controllers/bookingController');// do not need anymore, using stipes webhook 
 
 const router = express.Router();
 
-
-router.get('/',createBookingCheckout,isLoggedIn,getOverview );
-router.get('/tour/:slug',isLoggedIn,getTour );
-router.get('/login',isLoggedIn,Login );
+router.get('/', isLoggedIn, getOverview);
+router.get('/tour/:slug', isLoggedIn, getTour);
+router.get('/login', isLoggedIn, Login);
 
 // /login
-router.get('/signup',Signup)
-router.get('/me',protect,getAccount)
-router.get('/my-tours',protect,getMyTours)
+router.get('/signup', Signup);
+router.get('/me', protect, getAccount);
+router.get('/my-tours',
+    // createBookingCheckout, // do not need anymore, using stipes webhook 
+    protect,
+    getMyTours);
 
-router.post('/submit-user-data',protect,updateUserData)
+router.post('/submit-user-data', protect, updateUserData);
 
 module.exports = router;
