e1768dfb03565d9e0c9065a739fb9604157f864f
nika <nika3141535@gmail.com>
2024-07-20 11:52:38 +0400

    implemented stripe message

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
 /*eslint-disable*/
 
 const alertMessage = document.querySelector('body').dataset.alert
-if(alertMessage && alertMessage!= "") alert('success',alertMessage)
+if(alertMessage && alertMessage!= "") alert(alertMessage)
